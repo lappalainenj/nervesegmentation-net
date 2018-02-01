3d61138d9ae02c34b472ecb7b5e953ed496c8226
jkoal <lappalainenjk@gmail.com>
2018-02-01 19:20:40 +0100

    Added SegNet

--- dice_loss.py
+++ dice_loss.py
@@ -1,4 +1,5 @@
 from torch.nn.modules.loss import _Loss
+import numpy as np
 
 def _assert_no_grad(variable):
     assert not variable.requires_grad, \
@@ -6,43 +7,46 @@ def _assert_no_grad(variable):
         "mark these variables as not requiring gradients"
 
 class DiceLoss(_Loss):
-    def __init__(self, size_average=True, reduce=True, num_classes = 2, 
-                 classweights = [1, 1]):
+    def __init__(self, size_average=True, reduce=True, classweights = [1, 1]):
         super(DiceLoss, self).__init__(size_average)
         self.reduce = reduce
         self.cw = classweights
-        self.num_classes = num_classes
         
     def forward(self, input, target):
+        'Dice Loss calculated on Dualchannel Input and Singlechannel Target'
         _assert_no_grad(target)
         
         eps = 0.0000001
         
-        if self.num_classes == 2:
-            'Multiclass'
+        Closses = 0
+        for C in range(input.size(1)):
+            Cinput = input[:, C].contiguous()
+            target = target.squeeze()
             
-            Closses = 0
-            for C in range(input.size(1)):
-                Cinput = input[:, C].contiguous()
-                target = target.squeeze()
-                
-                assert Cinput.size() == target.size(), "Target size different to input size."
-                p  = Cinput.view(Cinput.size(0), -1).add(eps).float()
-                gt = target.view(target.size(0), -1).add(eps).float()
-                
-                if C == 0:
-                    gt = 1 - gt
-                numerator =  p.mul(gt).sum(1).mul(2)
-                denumerator = p.mul(p).sum(1).add(gt.mul(gt).sum(1))
-                if C == 0:
-                    Closses += self.cw[0]*(1-numerator.div(denumerator))
-                else:                    
-                    Closses += self.cw[1]*(1 - numerator.div(denumerator))
+            assert Cinput.size() == target.size(), "Target size different to input size."
+            p  = Cinput.view(Cinput.size(0), -1).add(eps).float()
+            gt = target.view(target.size(0), -1).add(eps).float()
+            
+            if C == 0:
+                gt = 1 - gt
+            numerator =  p.mul(gt).sum(1).mul(2)
+            denumerator = p.mul(p).sum(1).add(gt.mul(gt).sum(1))
+            if C == 0:
+                Closses += self.cw[0]*(1-numerator.div(denumerator))
+            else:                    
+                Closses += self.cw[1]*(1 - numerator.div(denumerator))
+
+        if self.size_average:
+            return Closses.mean()
+        return Closses.sum()
+        
     
-            if self.size_average:
-                return Closses.mean()
-            return Closses.sum()
-        'Singleclass'
+    def acc(self, input, target):
+        'Dice Accuracy calculated on Singlechannel Input&Target'
+        _assert_no_grad(target)
+        
+        eps = 0.0000001
+        
         assert input.size() == target.size(), "Target size different to input size."
         p  = input.view(input.size(0), -1).add(eps)
         gt = target.view(target.size(0), -1).add(eps)
@@ -51,8 +55,16 @@ class DiceLoss(_Loss):
         denumerator = p.mul(p).sum(1).add(gt.mul(gt).sum(1))
         
         if self.size_average:
-            return (1-numerator/denumerator).mean()
-        return (1-numerator/denumerator).sum()
+            return (numerator/denumerator).mean()
+        return (numerator/denumerator).sum()
+    
+    def dc_np(self, input, target):
+        '''Just to check if numpy returns the same'''
+        eps = 0.00000001
+        gt = target.reshape(target.shape[0], -1) + eps
+        p = input.reshape(input.shape[0], -1) + eps
+        dice = np.sum(p[gt==1])*2.0 / (np.sum(p*p) + np.sum(gt*gt))
+        return dice
         
         
         
